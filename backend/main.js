6597db8988cb5e77fd18f8a887e3d55150285b9c
buhleqampi <buhle3857@gmail.com>
2023-02-27 14:36:58 +0200

    This is my first backend commit

--- backend/main.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const route = require('./controller');
-const cors = require('cors');
-const port = parseInt(process.env.PORT) || 3000;
-const app = express();
-const {errorHandling} = require('./middleware/ErrorHandling');
---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const route = require('./controller');
+const cors = require('cors');
+const port = parseInt(process.env.PORT) || 3000;
+const app = express();
+const {errorHandling} = require('./middleware/ErrorHandling');
+const cookieParser = require('cookie-parser');
+
+app.use((req, res, next)=> {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
+        next();
+});
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json,
+    express.urlencoded({extended: false})
+)
+
+// Server is running
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+});
+// Handling all errors
+app.use(errorHandling);
